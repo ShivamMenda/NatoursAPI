da433f1727c0b86c39123b998ac73648fd89b610
Shivam Menda <shivammenda2002@gmail.com>
2024-01-09 14:40:15 +0530

    CRUD operations done.

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,79 +1,91 @@
 const express=require("express");
 const {Tour}= require("../models/tours");
 
-exports.checkBody=(req,res,next)=>{
-    if(!req.body.name || !req.body.price){
-        return res.status(400).json({
-            status:"fail",
-            message: "Bad request"
-        })
-    }
-    next();
-}
 
 exports.getAllTours= async (req,res)=>{
-    await Tour.find().then((tours)=>{
+    try {
+        let tours= await Tour.find();
         res.status(200).json({
             status:"Success",
-            data:{tours},
-            
+            length: tours.length,
+            data:{tours}
         })
-    }).catch((err)=>{
-        res.status(404).json({
+    } catch (err) {
+     res.status(404).json({
             status:"Fail",
             error:err.message,
-        })
-    })
+        })   
+    }
         
 };
 
 exports.getTour= async (req,res)=>{
-    await Tour.findById(req.params.id).then((doc)=>{
+    let {id}= req.params;
+    try {
+        let tour= await Tour.findById(id);
         res.status(200).json({
             status:"Success",
-            data: doc
-            
-        })
-    }).catch(err=>{
+            data: tour
+        });
+    } catch (err) {
         res.status(404).json({
             message:"Failure",
             error:err.message,
         })
-    })
-    
+    };
 };
 
 exports.createTour= async (req,res)=>{
-    let newTour= new Tour({
-        name: req.body.name,
-        rating: req.body.rating,
-        price:req.body.price
-    })
-    await newTour.save().then((doc)=>{
-        res.status(201).json({
+    let {name,rating,price} = req.body;
+    try {
+        let newTour= await Tour.create({
+        name: name,
+        rating: rating,
+        price:price
+    });
+    res.status(201).json({
             message:"success",
-            data:{tour:doc}
-        })
-    }).catch((err)=>{
+            data:{tour:newTour}
+        });
+    } catch (err) {
         res.status(400).json({
             message:"Failure",
             error:err.message,
         })
-    })
-        
-    
+    }   
 };
-exports.updateTour=(req,res)=>{
-    res.status(200).json({
-        status:"success patch",
+exports.updateTour=async (req,res)=>{
+    try {
+        const {id}=req.params
+        let updatedTour= await Tour.findByIdAndUpdate(id,req.body,{
+            new:true,
+            runValidators:true,
+        })
+        res.status(200).json({
+        status:"success",
         data:{
-            tour:"Updated Tour"
+            updatedTour
         }
     })
+    } catch (err) {
+       res.status(400).json({
+            message:"Failure",
+            error:err.message,
+        })  
+    }
 }
-exports.deleteTour=(req,res)=>{
-    res.status(204).json({
+exports.deleteTour= async (req,res)=>{
+    try {
+        let {id} = req.params
+        await Tour.findByIdAndDelete(id);
+        res.status(204).json({
         status:"success delete",
         data:null
     })
+    } catch (err) {
+        res.status(400).json({
+            message:"Failure",
+            error:err.message,
+        })  
+    }
 }
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dotenv=require("dotenv");
 const mongoose=require("mongoose");
 const express= require('express');
 const morgan=require("morgan")
-const router=require("./routes/tourRoutes")
+const tourRouter=require("./routes/tourRoutes")
 const app=express();
 
 dotenv.config({path:"./config.env"});
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-app.use("/api/v1/tours",router);
+app.use("/api/v1/tours",tourRouter);
 
 const port=process.env.PORT || 3000;
 app.listen(port,()=>{
